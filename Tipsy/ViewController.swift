5d098cab497fc4ae0d62c384e10aa03a8c6817c3
Zlata Guseva <gusevazlata96@gmail.com>
2022-09-04 22:41:15 +0400

    Create project

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Zlata Guseva on 04/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
